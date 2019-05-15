1adab572fa0fa2ec4fb78d74f49b5fc8c5125489
Gerry Chen <gdc9@duke.edu>
2019-05-15 19:25:04 -0400

    random

--- DynoData/MITSUBA/motorLossModel.m
+++ DynoData/MITSUBA/motorLossModel.m
@@ -1,7 +1,7 @@
 lossPoly_aeroAndBearing = [-1.06527e-08	-6.50352e-07	-1.02305e-04	1.12781e-03];
 lossPoly_eddy = [-1.11897e-08	-5.30369e-06	-6.07395e-03	3.03073e-02] - lossPoly_aeroAndBearing;
 
-RPM = 320;
+RPM = 250;
 
 loss = table();
 loss.types = {'aero and bearing';...
