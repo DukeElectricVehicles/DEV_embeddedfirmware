4362869e609b9056fd77459e3128835903bfcb33
Gerry Chen <gdc9@duke.edu>
2019-05-28 20:43:00 -0400

    random data - mostly junk, just redo it

--- DynoData/MITSUBA/12V_may28/dynoParse.m
+++ DynoData/MITSUBA/12V_may28/dynoParse.m
@@ -2,49 +2,57 @@ clear; clc; % close all;
 
 ROT_INERTIA = 0.8489;
 
-load ../spindown/spindown_noRotor_5 % PARASITIC LOSSES
-data = importdata('Gerry_0.txt');
+load ../spindown/spindown_noRotor_may27_before % PARASITIC LOSSES
+data = importdata('./12V_DEV_10advance_2.txt');
 
 data = data(data(:,2)>.1,:); % current > .1
+data = data(50:end,:);
 
+time = data(:,6)/1000;
+throttle = data(:, 5);
 voltage = data(:, 1);
 current = data(:, 2);
-rpm = smooth(data(:, 4);
-toShift = fix(1 / (min(rpm) * (data(2,6)-data(1,6))/1000 / 60));
-for i=1:length(rpm)-toShift % compensate for moving average having phase lag
-    rpm(i) = rpm(i+toShift);
-    toShift = fix(1 / (rpm(i) * (data(2,6)-data(1,6))/1000 / 60));
-end
+rpm = data(:, 4);
 
-% rpm = smooth(rpm, 54);
-rpm = smooth(rpm, 21);
+for i = 1:length(rpm) - 2%fix glitches in rpm readout
+   if (rpm(i) > 0) && (rpm(i+2) > 0) && (rpm(i+1) == 0)
+       rpm(i+1) = rpm(i);
+   end
+end
+glitches = find(abs(diff(rpm))>5);
+for glitch = glitches'
+    rpm(glitch+1) = rpm(glitch);%mean(rpm([glitch,glitch+2]));
+end
+rpm = 1./smooth(time,1./rpm, 54);
+rpm = circshift(rpm, -27);
+rpm(end-27:end) = rpm(end-28);
+
+rpm = smooth(time,rpm, 1001, 'sgolay', 5);
 
 omega = rpm * 2 * pi / 60;
 rpm_motor = rpm * 54/72;
 velo_mph = rpm_motor*(20/12/5280*pi)*60;
-throttle = data(:, 5);
-time = data(:, 6) ./ 1000;
 
 ePower = voltage .* current;
-% ePower = smooth(ePower, 50, 'sgolay');
+ePower = smooth(time,ePower, 150, 'sgolay');
 
 accel = gradient(omega)./gradient(time);
 
-% accel = smooth(accel, 101, 'sgolay');
+accel = smooth(time,accel, 401, 'sgolay');
 
 accelComp = accel - polyval(PARASITIC_LOSSES_ACC_OF_FLYWHEEL_RPS, omega);
 
 
 torque = ROT_INERTIA .* accelComp;
 mPower = torque .* omega;
-mPower = smooth(mPower, 50, 'sgolay');
+mPower = smooth(time,mPower, 150, 'sgolay');
 
 eff = mPower ./ ePower;
-eff = smooth(eff, 101, 'sgolay');
+eff = smooth(time,eff, 404, 'sgolay');
 
 figure(1);clf;
 subplot(3,1,1);
-plot(time, rpm_motor);
+plot(time, rpm_motor);%, time, data(:,4)*54/72);
 xlabel('Time (s)'); ylabel('Motor RPM');
 subplot(3,1,2);
 plot(rpm_motor, voltage, 'DisplayName','Voltage');
@@ -61,6 +69,12 @@ legend show;
 grid on;
 
 figure(2);clf;
+plot(rpm_motor, accelComp);
+xlabel('RPM'); ylabel('Acceleration');
+ylim([0,10]);
+grid on;
+
+figure(3);clf;
 plot(velo_mph, eff);
 ylim([0.6, 1]); xlim([0,max(velo_mph)]);
 ylabel('efficiency');
@@ -68,7 +82,7 @@ xlabel('speed (mph)');
 grid on;
 title('efficiency vs speed');
 
-figure(3);clf;
+figure(4);clf;
 plot(ePower, eff);
 ylim([0.6, 1]); xlim([0,100]);
 ylabel('efficiency');
